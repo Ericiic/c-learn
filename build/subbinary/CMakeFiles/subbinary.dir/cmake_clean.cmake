27b0d25a9e7c06c352063c36c2d6bc2675c0398e
luoxunhang <1842467410@qq.com>
2024-07-04 19:23:53 +0800

    first commit

---
+++ build/subbinary/CMakeFiles/subbinary.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/subbinary.dir/main.cpp.obj"
+  "CMakeFiles/subbinary.dir/main.cpp.obj.d"
+  "libsubbinary.dll.a"
+  "subbinary.exe"
+  "subbinary.exe.manifest"
+  "subbinary.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/subbinary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
